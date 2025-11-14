05ac6df47599283837a849fb1bfee4cfc9150d04
Bryan Espejo <u202213278@upc.edu.pe>
2025-11-14 02:19:23 -0500

    Enable Swagger in production

--- FuelTrack.API.Back/Program.cs
+++ FuelTrack.API.Back/Program.cs
@@ -42,7 +42,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FuelTrack.API.Back v1");
+        // c.RoutePrefix = string.Empty; // opcional si quisieras que Swagger sea la raíz
+    });
 }
 
 app.UseHttpsRedirection();
